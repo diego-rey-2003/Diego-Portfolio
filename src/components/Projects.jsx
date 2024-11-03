2a2c75147d39884ea5a5eb0e921b60fffc96da54
diego-rey-2003 <diego.rey2300@gmail.com>
2024-11-02 17:31:40 -0700

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { SectionWrapper } from '../hoc';
+import { styles } from '../styles';
+import { github, pineapple, pineappleHover } from '../assets';
+import { projects } from '../constants';
+import { fadeIn, textVariant, staggerContainer } from '../utils/motion';
+import HarvardSum from '../assets/projects/HarvardSum.pdf';
+
+
+const ProjectCard = ({
+  id,
+  name,
+  description,
+  image,
+  repo,
+  demo,
+  index,
+  active,
+  handleClick,
+}) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+      className={`relative ${
+        active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
+      } flex items-center justify-center min-w-[170px] 
+      h-[420px] cursor-pointer card-shadow overflow-hidden`}
+      onClick={() => handleClick(id)}>
+      <div
+        className="absolute top-0 left-0 z-10 bg-jetLight 
+      h-full w-full opacity-[0.5] rounded-[24px]"></div>
+
+      <img
+        src={image}
+        alt={name}
+        className="absolute w-full h-full object-cover rounded-[24px]"
+      />
+
+      {active !== id ? (
+        <div className="flex items-center justify-start pr-[4.5rem]">
+          <h3
+            className="font-extrabold font-beckman uppercase w-[200px] h-[30px] 
+        whitespace-nowrap sm:text-[27px] text-[18px] text-timberWolf tracking-[1px]
+        absolute z-0 lg:bottom-[7rem] lg:rotate-[-90deg] lg:origin-[0,0]
+        leading-none z-20">
+            {name}
+          </h3>
+        </div>
+      ) : (
+        <>
+           <div
+            className="absolute bottom-0 p-8 justify-start w-full 
+            flex-col bg-[rgba(122,122,122,0.5)] rounded-b-[24px] z-20">
+               <div className="absolute inset-0 flex justify-end m-3">
+           
+            {active === id && id === 'project-1' && (
+              <div
+                onClick={() => window.open(repo, '_blank')}
+                className="bg-night sm:w-11 sm:h-11 w-10 h-10 rounded-full 
+                  flex justify-center items-center cursor-pointer
+                  sm:opacity-[0.9] opacity-[0.8]">
+                <img
+                  src={github}
+                  alt="source code"
+                  className="w-4/5 h-4/5 object-contain"
+                />
+              </div>
+            )}
+            </div>
+
+            <h2
+              className="font-bold sm:text-[32px] text-[24px] 
+              text-timberWolf uppercase font-beckman sm:mt-0 -mt-[1rem]">
+              {name}
+            </h2>
+            <p
+              className="text-silver sm:text-[14px] text-[12px] 
+              max-w-3xl sm:leading-[24px] leading-[18px]
+              font-poppins tracking-[1px]">
+              {description}
+            </p>
+            <button
+              className="live-demo flex justify-between 
+              sm:text-[16px] text-[14px] text-timberWolf 
+              font-bold font-beckman items-center py-5 pl-2 pr-3 
+              whitespace-nowrap gap-1 sm:w-[138px] sm:h-[50px] 
+              w-[125px] h-[46px] rounded-[10px] glassmorphism 
+              sm:mt-[22px] mt-[16px] hover:bg-battleGray 
+              hover:text-eerieBlack transition duration-[0.2s] 
+              ease-in-out"
+              onClick={() => {
+                if (id === 'project-1') {
+                  window.open(demo, '_blank');
+                } else if (id === 'project-2') {
+                  window.open(HarvardSum, '_blank'); // Ensure you have imported HarvardSum correctly
+                } else if (id === 'project-3') {
+                  window.open('https://www.youtube.com/watch?v=Abzka1Uw9EU&list=PLROWtI7htoKQopkDBCo5bz79M-w0orAY4&index=3&t=1s', '_blank');
+                }
+              }}
+              onMouseOver={() => {
+                document
+                  .querySelector('.btn-icon')
+                  .setAttribute('src', pineappleHover);
+              }}
+              onMouseOut={() => {
+                document
+                  .querySelector('.btn-icon')
+                  .setAttribute('src', pineapple);
+              }}>
+              <img
+                src={pineapple}
+                alt="pineapple"
+                className="btn-icon sm:w-[34px] sm:h-[34px] 
+                  w-[30px] h-[30px] object-contain"
+              />
+              {id === 'project-1' ? 'LIVE DEMO' : 'details'}
+            </button>
+          </div>
+        </>
+      )}
+    </motion.div>
+  );
+};
+
+
+const Projects = () => {
+  const [active, setActive] = useState('project-2');
+
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} `}>Here are my</p>
+        <h2 className={`${styles.sectionHeadTextLight}`}>Projects</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn('', '', 0.1, 1)}
+          className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px] text-justify">
+      This page details my project experience, highlighting my work on real-world 
+      challenges and the steps I took to approach each. One code repository linked 
+      and other projects explained in full shows my adaptability, technical insight,
+       and commitment to efficient problem-solving.
+        </motion.p>
+      </div>
+
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}>
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {projects.map((project, index) => (
+            <ProjectCard
+              key={project.id}
+              index={index}
+              {...project}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Projects, 'projects');
---
+++ src/constants/index.js
@@ -0,0 +1,208 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Electrical Engineer',
+    icon: backend,
+  },
+  {
+    title: 'Researcher',
+    icon: ux,
+  },
+  {
+    title: 'Resident Assistant',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Rails',
+    icon: rubyrails,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Resident Assistant',
+    company_name: 'Residential Life',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Aug 2024 - present',
+  },
+
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'SF Maps',
+    description: 'A web application for San Francisco State students to locate their classes. Awarded "Best Design"',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/Alex-RV/sfsu_maps',
+    demo: 'https://sfsu-maps.vercel.app/',
+  },
+  {
+    id: 'project-2',
+    name: 'Harvard SUmmer Camp',
+    description:
+      'Designed and build a functional robot that won second place in the competition.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    demo: 'https://shaqdeff.github.io/Leaderboard/',
+  },
+  {
+    id: 'project-3',
+    name: 'Not to persuade',
+    description: 'Developed an algorithm to prevent social media harassment. Secured first place in Hackathon',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    
+    demo: 'https://www.youtube.com/watch?v=Abzka1Uw9EU&list=PLROWtI7htoKQopkDBCo5bz79M-w0orAY4&index=3&t=1s',
+  },
+ 
+];
+
+export { services, technologies, experiences, projects };
